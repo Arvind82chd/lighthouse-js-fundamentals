4be9f8988ab2a884a9766cf9ff2921b69dc6771d
Arvind Sharma <mail.arvind.s@gmail.com>
2021-10-30 17:32:27 +0000

    test

---
+++ goodStations.js
@@ -0,0 +1,32 @@
+const stations = [
+    ['Big Bear Donair', 10, 'restaurant'],
+    ['Bright Lights Elementary', 50, 'school'],
+    ['Moose Mountain Community Centre', 45, 'community centre']
+  ];
+  const chooseStations = function(stations) {
+  const goodStations = [];
+    for (let station of stations) {
+      const capacity = station[1];
+      if (capacity >= 20) {
+         /* const type = station[2]; 
+          if ( type === 'school' || type === 'community center') { */ 
+              goodStations.push(station[0]);
+            //}
+        }
+    }
+
+    return goodStations
+  }
+  console.log(chooseStations(stations));
+
+ /* const chooseStations = function(stations) {
+    const goodStations = [];
+    for (let station of stations) {
+      const capacity = station[1];
+      if (capacity >= 20) {
+        goodStations.push(station[0]);
+      }
+    }
+    return goodStations;
+  };
+  console.log(chooseStations());
